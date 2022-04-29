80c99699aeba8a2e41fc2d146a187c5147caf935
Joshua Licudo <joshua.licudo.753@my.csun.edu>
2022-04-28 18:52:20 -0700

    Extend Item abstract class with OnTriggerEnter logic from ArcadeKartPowerup
    Also import free rocket asset from unity store

--- Assets/Karting/Scripts/KartSystems/Items/Item.cs
+++ Assets/Karting/Scripts/KartSystems/Items/Item.cs
@@ -4,6 +4,23 @@ namespace KartGame.KartSystems.Items
 {
     public abstract class Item : MonoBehaviour
     {
+        public GameObject item;
         public abstract void activate(Racer racer);
+
+        public abstract void pickup(Racer racer);
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var rb = other.attachedRigidbody;
+            if (rb)
+            {
+                var racer = rb.GetComponent<Racer>();
+                if (racer)
+                {
+                    this.pickup(racer);
+                    this.gameObject.SetActive(false);
+                }
+            }
+        }
     }
 }
