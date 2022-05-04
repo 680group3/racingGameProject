0485486ab04ffeedef7eabb10c56040fa8e4b4dc
Joshua Licudo <joshua.licudo.753@my.csun.edu>
2022-05-03 19:02:03 -0700

    Duct-tape Photon networking objects to rocket prefabs

--- Assets/Karting/Scripts/KartSystems/Items/NormalRocketItem.cs
+++ Assets/Karting/Scripts/KartSystems/Items/NormalRocketItem.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
+using Photon.Pun;
 
 namespace KartGame.KartSystems.Items
 {
     public class NormalRocketItem : Item
     {
+
         public override void activate(Racer racer)
         {
-            GameObject prefab = Resources.Load<GameObject>("Assets/AurynSky/Rockets Missiles and Bombs/Prefabs/Green/Rocket16_Green.prefab");
-            Instantiate(prefab, racer.getMrig().transform.position, Quaternion.identity);
+            GameObject prefab = Resources.Load("Rocket16_Green") as GameObject;
+            PhotonNetwork.Instantiate(prefab.name, racer.getMrig().transform.position, Quaternion.identity, 0);
         }
 
         public override void pickup(Racer racer)
